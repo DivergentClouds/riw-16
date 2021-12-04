4e3fad8626a7d562421ac1e69545ee719bc536eb
DivergentClouds <27089165+DivergentClouds@users.noreply.github.com>
2021-12-04 14:11:10 -0600

    Commented A.asm

--- examples/A.asm
+++ examples/A.asm
@@ -1,9 +1,9 @@
-byte	$1,		41h
-byte	$2,		1
-sub		$3,		$3,		$3
-sub		$2,		$3,		$2
-store	$2,		$3,		$1
-byte	$1,		0Ah
-store	$2,		$3,		$1
-halt
+byte  $1,    41h        ; Load ASCII 'A' into register 1
+byte  $2,    1          ; Load 1 into register 2
+sub   $0,    $0,    $0  ; Set register 0 to 0 
+sub   $2,    $0,    $2  ; Make register 2 point to the top of memory
+store $2,    $0,    $1  ; Write the contents of register 1 to char-out
+byte  $1,    0Ah        ; Load ASCII newline into register 1
+store $2,    $0,    $1  ; Write the contents of register 1 to char-out
+halt                    ; Stop the program
 
