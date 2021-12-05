b72634c0506e406f29c5f1b93a3832880f612098
DivergentClouds <27089165+DivergentClouds@users.noreply.github.com>
2021-12-04 20:39:00 -0600

    updated A.asm to use octet instead of byte

--- examples/A.asm
+++ examples/A.asm
@@ -1,9 +1,9 @@
-byte  $1,    41h        ; Load ASCII 'A' into register 1
-byte  $2,    1          ; Load 1 into register 2
-sub   $0,    $0,    $0  ; Set register 0 to 0 
-sub   $2,    $0,    $2  ; Make register 2 point to the top of memory
-store $2,    $0,    $1  ; Write the contents of register 1 to char-out
-byte  $1,    0Ah        ; Load ASCII newline into register 1
-store $2,    $0,    $1  ; Write the contents of register 1 to char-out
-halt                    ; Stop the program
-
+octet   $1,    41h            ; load ascii 'a' into register 1
+octet   $2,    1              ; load 1 into register 2
+sub     $0,    $0,    $0      ; set register 0 to 0 
+sub     $2,    $0,    $2      ; make register 2 point to the top of memory
+store   $2,    $0,    $1      ; write the contents of register 1 to char-out
+octet   $1,    0ah            ; load ascii newline into register 1
+store   $2,    $0,    $1      ; write the contents of register 1 to char-out
+halt                          ; stop the program
+ 
